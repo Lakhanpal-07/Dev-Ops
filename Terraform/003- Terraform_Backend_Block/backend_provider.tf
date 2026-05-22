32cf00f4a372bfb64e310754bb2d316868773964
Lakhanpal-07 <nakesh.lakhanpal@gmail.com>
2026-05-22 12:13:29 +0530

    backend updated

---
+++ Terraform/003- Terraform_Backend_Block/backend_provider.tf
@@ -0,0 +1,17 @@
+
+# Terraform Backend Block and Provider Configuration
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg01"
+    storage_account_name = "stterraformstate"
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
+  }
+}
