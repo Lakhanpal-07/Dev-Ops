1c7b9bbb963833218c04ea3d43d0c1c96975e07b
Lakhanpal-07 <nakesh.lakhanpal@gmail.com>
2026-05-22 18:47:46 +0530

    dependencies updtaed

--- Terraform/006- Terraform_Explicit_dependency/main.tf
+++ Terraform/006- Terraform_Explicit_dependency/main.tf
@@ -1,16 +1,20 @@
+
 #   resource group for stoarge account
 
-resource "azurerm_resource_group" "rg02"{
-  name     = "rg02"
+resource "azurerm_resource_group"  "rg03" {
+  name     = "rg03"
   location = "eastus"
 }
 
 #   storage account for the project
 
 resource "azurerm_storage_account" "stg02" {
+
+  depends_on = [azurerm_resource_group.rg03]
+
   name                     = "stg02"
-  resource_group_name      = azurerm_resource_group.rg02.name
-  location                 = azurerm_resource_group.rg02.location
+  resource_group_name      = "rg03"
+  location                 = "eastus"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
