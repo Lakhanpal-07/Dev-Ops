c90f46761ada5a14fd4d156511e2e79ad66c4db3
Lakhanpal-07 <nakesh.lakhanpal@gmail.com>
2026-05-27 01:03:20 +0530

    code updated

---
+++ Terraform/011-Terraform_Module/resource_resource_group/output.tf
@@ -0,0 +1,13 @@
+output "rg_name" {
+  value = azurerm_resource_group.rg_name
+}
+
+output "rg_location" {
+  value = azurerm_resource_group.rg.location
+
+}
+
+output "rg_id" {
+
+  value = azurerm_resource_group.rg.id
+}
