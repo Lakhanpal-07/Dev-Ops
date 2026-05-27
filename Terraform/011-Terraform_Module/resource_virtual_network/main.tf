7e5ef750d7f20bf87954b354a5d5aa226eeaccfb
Lakhanpal-07 <nakesh.lakhanpal@gmail.com>
2026-05-27 16:31:23 +0530

    changes done

--- Terraform/011-Terraform_Module/resource_virtual_network/main.tf
+++ Terraform/011-Terraform_Module/resource_virtual_network/main.tf
@@ -1,9 +1,20 @@
-
+resource "azurerm_resource_group" "rg001" {
+  name = "rg_001"
+  location  ="westus"
+}
 
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet01"
-  location            = data.terraform_remote_state.rg.output.rg_location
-  resource_group_name = data.terraform_remote_state.output.rg_name
+  location            = azurerm_resource_group.rg001.location
+  resource_group_name = azurerm_resource_group.rg001.name
   address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "subnet_1" {
+  name  = "subnet1"
+  resource_group_name = azurerm_resource_group.rg001.name
+virtual_network_name = azurerm_virtual_network.vnet.name
+address_prefixes = ["10.0.1.0/24 "]
+}
+ 
