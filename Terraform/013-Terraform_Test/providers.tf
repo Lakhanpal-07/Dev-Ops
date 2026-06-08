b69811f3dd641b7c46d617b09b1f3e5f32dd714d
Lakhanpal-07 <nakesh.lakhanpal@gmail.com>
2026-06-08 23:39:41 +0530

    provider block updates

--- Terraform/013-Terraform_Test/providers.tf
+++ Terraform/013-Terraform_Test/providers.tf
@@ -1,3 +1,5 @@
+
+# provider block is used to specify the provider and its configuration. In this case, we are using the Azure Resource Manager (azurerm) provider to manage resources in Microsoft Azure.
 terraform {
   required_providers {
     azurerm = {
