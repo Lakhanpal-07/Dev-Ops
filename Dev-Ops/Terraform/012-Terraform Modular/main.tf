cf0ea526fb3358391fbb43fb3f9dd0c7bb77a0d4
pal_07 <nakesh.lakhanpal@gmail.com>
2026-07-10 11:30:19 +0530

    Uncomment Terraform module configurations

--- Dev-Ops/Terraform/012-Terraform Modular/main.tf
+++ Dev-Ops/Terraform/012-Terraform Modular/main.tf
@@ -1,22 +1,22 @@
-# # module "resource_group" {
-# #   source = ./modules/resource_group
+ module "resource_group" {
+   source = ./modules/resource_group
 
-# #   name     = "rg-terraform-nested-map"
-# #   location = "eastus"
-# # }
+   name     = "rg-terraform-nested-map"
+   location = "eastus"
+ }
 
 
-# module "storage_account" {
-#   source = "./modules/storage_account"
+ module "storage_account" {
+   source = "./modules/storage_account"
 
-#   mystg = {
-#     "stg01" = {
-#       name                     = "mystg01"
-#       resource_group_name      = module.resource_group.name
-#       location                 = module.resource_group.location
-#       account_tier             = "Standard"
-#       account_replication_type = "LRS"
+   mystg = {
+     "stg01" = {
+       name                     = "mystg01"
+       resource_group_name      = module.resource_group.name
+       location                 = module.resource_group.location
+       account_tier             = "Standard"
+       account_replication_type = "LRS"
 
-#     }
-#   }
-# }
+     }
+   }
+ }
