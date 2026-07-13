d2ee6c4db64c829c07bc6fdd11a1a3253391c83b
Lakhanpal-07 <nakesh.lakhanpal@gmail.com>
2026-07-13 11:57:42 +0530

    trash cleansing

--- Dev-Ops/Terraform/014-Terraform-Assingned/az-loadbalancer/output.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "Load Balancer ID"
-  value       = azurerm_lb.lb.id
-}
-
-output "frontend_ip" {
-  description = "Frontend Public IP of Load Balancer"
-  value       = azurerm_public_ip.lb_pip.ip_address
-}
